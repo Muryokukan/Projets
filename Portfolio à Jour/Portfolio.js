a733e007b2289f9590149d76c20237c7f5c80c42
Cédric <134500848+Muryokukan@users.noreply.github.com>
2024-03-07 18:24:20 +0100

    Portfolio à jour

--- Portfolio à Jour/Portfolio.js
+++ Portfolio à Jour/Portfolio.js
@@ -37,13 +37,11 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
 reveal();
 
-// Get the button:
+
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -54,8 +52,23 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE et Opera
+}
+
+document.getElementById('votreLien').addEventListener('click', function(event) {
+  event.preventDefault(); 
+  var text = 'c.limosin@outlook.fr'; // Texte que vous voulez copier
+  copyToClipboard(text); // Appel de la fonction pour copier le texte dans le presse-papiers
+});
+
+function copyToClipboard(text) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  alert('E-mail copié dans le presse-papiers : ' + text);
 }
